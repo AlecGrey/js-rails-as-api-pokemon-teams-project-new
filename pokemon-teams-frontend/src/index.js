0aa546d70f76ec4bb1f0e6364f29751df183868d
AlecGrey <alecgrey.coding@gmail.com>
2020-11-20 14:09:53 -0800

    all edits made as a group

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,67 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    getAllTrainers()
+
+})
+
+function getAllTrainers() {
+    fetch(TRAINERS_URL)
+        .then(resp => resp.json())
+        .then(json => {
+            generateAllTrainers(json)
+        })
+}
+
+function generateAllTrainers(trainerArray) {
+    // create all trainer cards, then append to the main div, defined above
+    trainerArray.forEach( trainer => {
+        // console.log(trainer)
+        const trainerCard = createTrainerCard(trainer)
+        main.appendChild(trainerCard)
+    } )
+}
+
+function createTrainerCard(trainer) {
+    // create AND RETURN a trainer card
+    // console.log(trainer.id)
+    const card = document.createElement('div')
+    card.className = 'card'
+    card.dataset.id = trainer.id
+    
+    const p = document.createElement('p')
+    p.textContent = trainer.name
+
+    const button = document.createElement('button')
+    button.textContent = "Add Pokemon"
+    // how do trainer-id ? 
+
+    const ul = document.createElement('ul')
+
+    for (const poke of trainer.pokemons) {
+        const pokeItem = createPokemonListItem(poke)
+        ul.appendChild(pokeItem)
+    }
+
+    card.append(p, button, ul)
+    return card
+}
+
+function createPokemonListItem(pokeObj) {
+    // create AND RETURN a pokemon list item
+
+    const listItem = document.createElement('li')
+    listItem.textContent = `${ pokeObj.nickname } (${ pokeObj.species }) `
+
+    const pokeButton = document.createElement('button')
+    pokeButton.className = 'release'
+    pokeButton.dataset.pokemonID = pokeObj.id
+    pokeButton.textContent = 'Release'
+
+    listItem.appendChild(pokeButton)
+    return listItem
+}
